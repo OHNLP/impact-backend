b01c2a0b257841b8c482aa84ca611424b20eb471
Andrew Wen <2060andrew@gmail.com>
2022-09-02 09:37:13 -0500

    Add lab/observation value coercion

--- src/main/java/org/ohnlp/ir/cat/ehr/datasource/EHRDataSource.java
+++ src/main/java/org/ohnlp/ir/cat/ehr/datasource/EHRDataSource.java
@@ -1,5 +1,6 @@
 package org.ohnlp.ir.cat.ehr.datasource;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.values.PCollection;
 import org.hl7.fhir.r4.model.*;
@@ -9,6 +10,7 @@ import org.ohnlp.ir.cat.structs.ClinicalDataType;
 import java.util.Set;
 
 public interface EHRDataSource {
+    void loadConfig(JsonNode node);
     Set<CriterionValue> convertToLocalTerminology(ClinicalDataType type, CriterionValue input);
     PCollection<Condition> getConditions(Pipeline pipeline);
     PCollection<MedicationStatement> getMedications(Pipeline pipeline);
--- src/main/java/org/ohnlp/ir/cat/scoring/Scorer.java
+++ src/main/java/org/ohnlp/ir/cat/scoring/Scorer.java
@@ -1,7 +1,15 @@
 package org.ohnlp.ir.cat.scoring;
 
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.ohnlp.ir.cat.criteria.CriterionValue;
 import org.ohnlp.ir.cat.ehr.datasource.EHRDataSource;
 import org.ohnlp.ir.cat.structs.ClinicalDataType;
+import org.ohnlp.ir.cat.structs.PatientScore;
+
+import java.util.Map;
+import java.util.Set;
 
 public abstract class Scorer {
 
@@ -12,4 +20,6 @@ public abstract class Scorer {
         this.dataSource = dataSource;
         this.queryType = queryType;
     }
+
+    public abstract PCollection<KV<String, PatientScore>> score(Pipeline p, Map<String, Set<CriterionValue>> query);
 }
