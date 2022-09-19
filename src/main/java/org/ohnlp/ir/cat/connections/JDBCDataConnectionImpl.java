ea8614ea49378710493be1e667a91d7ad8defe78
Andrew Wen <2060andrew@gmail.com>
2022-09-18 21:30:40 -0500

    PreparedStatement#setObject does not autodetect JODA datatypes

--- src/main/java/org/ohnlp/ir/cat/connections/JDBCDataConnectionImpl.java
+++ src/main/java/org/ohnlp/ir/cat/connections/JDBCDataConnectionImpl.java
@@ -6,10 +6,12 @@ import org.apache.beam.sdk.io.jdbc.JdbcIO;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.Row;
+import org.joda.time.ReadableDateTime;
 import org.ohnlp.ir.cat.DataConnection;
 
 import java.sql.PreparedStatement;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -43,7 +45,11 @@ public class JDBCDataConnectionImpl implements DataConnection {
         // And now dynamically write rows using setObject type inferences
         data.apply("Write to JDBC", JdbcIO.<Row>write().withStatement(insertPs).withPreparedStatementSetter((r, preparedStatement) -> {
             for (int i = 0; i < fields.length; i++) {
-                preparedStatement.setObject(i + 1, r.getValue(fields[i]));
+                Object value = r.getValue(fields[i]);
+                if (value instanceof ReadableDateTime) {
+                    value = new Date(((ReadableDateTime) value).getMillis());
+                }
+                preparedStatement.setObject(i + 1, value); // JDBC driver should dynamically determine type at runtime to insert
             }
         }));
     }
